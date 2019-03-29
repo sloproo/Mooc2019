4701475684cd60d52d22b4d4576365b97f0558f2
Pyry <Pyry@mustis>
2019-03-29 13:31:16 +0200

    Juuh. Valmis on, testaillaan repon luomista ja tallentamista

---
+++ src/main/java/lentokentta/Main.java
@@ -0,0 +1,15 @@
+package lentokentta;
+import lentokentta.logiikka.Hallinta;
+import lentokentta.ui.Ui;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Kirjoita pääohjelma tänne. Omien luokkien tekeminen on hyödyllistä.
+        Scanner lukija = new Scanner(System.in);
+        Ui ui = new Ui(new Hallinta(), lukija);
+        ui.kaynnista();
+    }
+}
