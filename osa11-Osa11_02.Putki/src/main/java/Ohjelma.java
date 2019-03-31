00eb700d4cf14da03a62deb88584aafd2f14597a
Pyry <Pyry@mustis>
2019-04-01 00:55:43 +0300

    Eka testaus

--- osa11-Osa11_02.Putki/src/main/java/Ohjelma.java
+++ osa11-Osa11_02.Putki/src/main/java/Ohjelma.java
@@ -6,6 +6,13 @@ public class Ohjelma {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
         // voit testata luokkaasi täällä
-
+        Putki<String> putki = new Putki<>();
+        putki.lisaaPutkeen("dibi");
+        putki.lisaaPutkeen("dab");
+        putki.lisaaPutkeen("dab");
+        putki.lisaaPutkeen("daa");
+        while(putki.onkoPutkessa()) {
+            System.out.println(putki.otaPutkesta());
+        }
     }
 }
