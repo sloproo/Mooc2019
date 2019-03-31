cfd2d01388b49e97cc0bb18dd6e1166854d9767f
Pyry <Pyry@mustis>
2019-04-01 00:56:41 +0300

    Toka testaus

--- osa11-Osa11_02.Putki/src/main/java/Ohjelma.java
+++ osa11-Osa11_02.Putki/src/main/java/Ohjelma.java
@@ -6,13 +6,16 @@ public class Ohjelma {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
         // voit testata luokkaasi täällä
-        Putki<String> putki = new Putki<>();
-        putki.lisaaPutkeen("dibi");
-        putki.lisaaPutkeen("dab");
-        putki.lisaaPutkeen("dab");
-        putki.lisaaPutkeen("daa");
-        while(putki.onkoPutkessa()) {
-            System.out.println(putki.otaPutkesta());
-        }
+        Putki<Integer> numeroputki = new Putki<>();
+numeroputki.lisaaPutkeen(1);
+numeroputki.lisaaPutkeen(2);
+numeroputki.lisaaPutkeen(3);
+
+int summa = 0;
+while(numeroputki.onkoPutkessa()) {
+    summa = summa + numeroputki.otaPutkesta();
+}
+System.out.println(summa);
+System.out.println(numeroputki.otaPutkesta());
     }
 }
