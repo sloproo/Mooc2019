73dc173a81309116c21e0018038b0d60ed3b4b3a
Pyry <Pyry@mustis>
2019-04-01 01:30:36 +0300

    Ratkaistu.

--- osa11-Osa11_03.RajatutTaulukossa/src/main/java/Ohjelma.java
+++ osa11-Osa11_03.RajatutTaulukossa/src/main/java/Ohjelma.java
@@ -6,7 +6,27 @@ public class Ohjelma {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
         // voit testata luokkaasi täällä
+        int[] luvut = {3, -1, 8, 4};
 
+System.out.println(summa(luvut, 0, 0, 0, 0));
+System.out.println(summa(luvut, 0, 0, 0, 10));
+System.out.println(summa(luvut, 0, 1, 0, 10));
+System.out.println(summa(luvut, 0, 1, -10, 10));
+System.out.println(summa(luvut, -1, 999, -10, 10));
+
+    }
+    
+    public static int summa(int[] taulukko, int mista, int mihin, int pienin, 
+            int suurin) {
+        int palautettavaSumma = 0;
+        if (mista < 0) mista = 0;
+        if (mihin > taulukko.length - 1) mihin = taulukko.length -1;
+        for (int i = mista; i <= mihin; i++) {
+            if (taulukko[i] >= pienin && taulukko[i] <= suurin) 
+                palautettavaSumma += taulukko[i];
+        }
+        return palautettavaSumma;
+        
     }
 
 }
