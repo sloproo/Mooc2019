9c2039add53f886676dd03b084b9ef06189bd0aa
Pyry <Pyry@mustis>
2019-04-12 19:05:48 +0300

    Kolmas done

--- osa12-Osa12_03.NappiJaTekstikentta/src/main/java/nappijatekstikentta/NappiJaTekstikenttaSovellus.java
+++ osa12-Osa12_03.NappiJaTekstikentta/src/main/java/nappijatekstikentta/NappiJaTekstikenttaSovellus.java
@@ -1,11 +1,32 @@
 package nappijatekstikentta;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class NappiJaTekstikenttaSovellus {
+public class NappiJaTekstikenttaSovellus extends Application {
 
+    @Override
+    public void start(Stage ikkuna) {
+        Button nappi = new Button("Nappeja ko nappikaupassa");
+        TextField tekstikentta = new TextField();
+        
+        FlowPane komponenttiryhma = new FlowPane();
+        komponenttiryhma.getChildren().add(nappi);
+        komponenttiryhma.getChildren().add(tekstikentta);
+        
+        Scene nakyma = new Scene(komponenttiryhma);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+        
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NappiJaTekstikenttaSovellus.class);
     }
 
 }
