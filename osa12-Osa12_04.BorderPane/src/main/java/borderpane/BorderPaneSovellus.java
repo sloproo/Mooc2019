c7b2364a2b1c9db0eafdd928cf83a28ea348d0c4
Pyry <Pyry@mustis>
2019-04-14 17:12:01 +0300

    Neljäs tehty

--- osa12-Osa12_04.BorderPane/src/main/java/borderpane/BorderPaneSovellus.java
+++ osa12-Osa12_04.BorderPane/src/main/java/borderpane/BorderPaneSovellus.java
@@ -1,11 +1,31 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class BorderPaneSovellus {
+
+public class BorderPaneSovellus extends Application {
+    
+    @Override
+    public void start(Stage ikkuna) {
+        BorderPane asettelu = new BorderPane();
+        asettelu.setTop(new Label("NORTH"));
+        asettelu.setRight(new Label("EAST"));
+        asettelu.setBottom(new Label("SOUTH"));
+        
+        Scene nakyma = new Scene(asettelu);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+    }
+    
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(BorderPaneSovellus.class);
     }
 
 }
