9446a855117d306114a1f599855ec743c3148609
Pyry <Pyry@mustis>
2019-04-14 18:36:15 +0300

    6: ilmoitin done

--- osa12-Osa12_06.Ilmoitin/src/main/java/ilmoitin/IlmoitinSovellus.java
+++ osa12-Osa12_06.Ilmoitin/src/main/java/ilmoitin/IlmoitinSovellus.java
@@ -1,11 +1,39 @@
 package ilmoitin;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class IlmoitinSovellus {
+
+
+public class IlmoitinSovellus extends Application {
+    
+    @Override
+    public void start(Stage ikkuna) {
+        TextField tekstikentta = new TextField();
+        Button nappi = new Button("Päivitä");
+        Label tekstikomponentti = new Label();
+        
+        nappi.setOnAction((event) -> {
+            tekstikomponentti.setText(tekstikentta.getText());
+        });
+
+        VBox asettelu = new VBox();
+        asettelu.getChildren().addAll(tekstikentta, nappi, tekstikomponentti);
+        
+        Scene nakyma = new Scene(asettelu);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+    }
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(IlmoitinSovellus.class);
     }
 
 }
