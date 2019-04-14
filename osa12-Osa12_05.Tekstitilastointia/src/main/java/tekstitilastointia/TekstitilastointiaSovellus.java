7bd0c496d9f4661c1d6b3e1c6c3caa7c4013e183
Pyry <Pyry@mustis>
2019-04-14 17:33:19 +0300

    red five I'm going in

--- osa12-Osa12_05.Tekstitilastointia/src/main/java/tekstitilastointia/TekstitilastointiaSovellus.java
+++ osa12-Osa12_05.Tekstitilastointia/src/main/java/tekstitilastointia/TekstitilastointiaSovellus.java
@@ -1,11 +1,44 @@
 package tekstitilastointia;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TekstitilastointiaSovellus {
+
+
+
+
+public class TekstitilastointiaSovellus extends Application {
+    
+    @Override
+    public void start(Stage ikkuna) {
+        BorderPane asettelu = new BorderPane();
+        
+        TextArea tekstikentta = new TextArea();
+        
+        HBox tekstikomponentit = new HBox();
+        tekstikomponentit.setSpacing(10);
+        tekstikomponentit.getChildren().add(new Label("Kirjaimia: 0"));
+        tekstikomponentit.getChildren().add(new Label("Sanoja: 0"));
+        tekstikomponentit.getChildren().add(new Label("Pisin sana on:"));
+        
+        asettelu.setCenter(tekstikentta);
+        asettelu.setBottom(tekstikomponentit);
+        
+        Scene nakyma = new Scene(asettelu);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+        
+    }
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TekstitilastointiaSovellus.class);
     }
 
 }
