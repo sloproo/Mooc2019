67aaa32e15834dbdd4060a8017a0f18197baacd6
Pyry <Pyry@mustis>
2019-04-15 00:54:21 +0300

    Vaali-illan päätteeksi pari pikaista vielä

--- osa12-Osa12_08.KayttajanOtsikko/src/main/java/otsikko/KayttajanOtsikko.java
+++ osa12-Osa12_08.KayttajanOtsikko/src/main/java/otsikko/KayttajanOtsikko.java
@@ -1,7 +1,16 @@
 package otsikko;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class KayttajanOtsikko {
+public class KayttajanOtsikko extends Application {
+
+    @Override
+    public void start(Stage ikkuna) {
+        ikkuna.setTitle(getParameters().getNamed().get("title"));
+        ikkuna.show();
+    }
+    
 
 
 }
--- osa12-Osa12_08.KayttajanOtsikko/src/main/java/otsikko/Main.java
+++ osa12-Osa12_08.KayttajanOtsikko/src/main/java/otsikko/Main.java
@@ -1,11 +1,15 @@
 package otsikko;
 
+import java.util.Scanner;
+import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        System.out.println("Voisitko olla niin ystävällinen, että antaisit "
+                + "luotavan ikkunan otsikon, kiitos? ");
+        String ikkunanNimi = new Scanner(System.in).nextLine();
+        Application.launch(KayttajanOtsikko.class, "--title=" + ikkunanNimi);
     }
 
 }
