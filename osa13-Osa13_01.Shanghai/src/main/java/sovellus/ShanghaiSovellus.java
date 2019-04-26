8aec49c3cf8d1cbfb53d127586342a49303f8cbe
Pyry <Pyry@mustis>
2019-04-26 18:27:08 +0300

    Ekat tehty

--- osa13-Osa13_01.Shanghai/src/main/java/sovellus/ShanghaiSovellus.java
+++ osa13-Osa13_01.Shanghai/src/main/java/sovellus/ShanghaiSovellus.java
@@ -1,9 +1,69 @@
 package sovellus;
 
-public class ShanghaiSovellus {
+import java.io.File;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.chart.NumberAxis;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
+
+
+public class ShanghaiSovellus extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ShanghaiSovellus.class);
+    }
+    
+    @Override
+    public void start(Stage ikkuna) {
+        NumberAxis xAkseli = new NumberAxis(2005, 2020, 1);
+        NumberAxis yAkseli = new NumberAxis();
+        
+        xAkseli.setLabel("Vuosi");
+        yAkseli.setLabel("Sijoitus");
+        
+        LineChart<Number, Number> viivakaavio = new LineChart<>(xAkseli, yAkseli);
+        
+        /*String data = "";
+        try (Scanner tiedostonlukija = new Scanner(new File("shanghai_HY.txt"))) {
+            while (tiedostonlukija.hasNextLine()) {
+                data += tiedostonlukija.nextLine() + " ";
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Virhe: " + e.getMessage());
+        }
+        */
+        
+        String data = "2007 73 " +
+"2008 68 " +
+"2009 72 " +
+"2010 72 " +
+"2011 74 " +
+"2012 73 " +
+"2013 76 " +
+"2014 73 " +
+"2015 67 " +
+"2016 56 " +
+"2017 56 ";
+        
+        XYChart.Series HYShanghai = new XYChart.Series();
+        HYShanghai.setName("Helsingin yliopisto");
+        
+        String[] dataTaulukko = data.split(" ");
+        for (int i = 0; i < dataTaulukko.length; i += 2) {
+            HYShanghai.getData().add(new XYChart.Data(Integer.valueOf(dataTaulukko[i]), Integer.valueOf(dataTaulukko[i+1])));
+        }
+        
+        viivakaavio.getData().add(HYShanghai);
+        
+        Scene nakyma = new Scene(viivakaavio);
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+        
     }
 
 }
--- osa13-Osa13_02.Puolueet/src/main/java/sovellus/PuolueetSovellus.java
+++ osa13-Osa13_02.Puolueet/src/main/java/sovellus/PuolueetSovellus.java
@@ -1,9 +1,67 @@
 package sovellus;
 
-public class PuolueetSovellus {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class PuolueetSovellus extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(PuolueetSovellus.class);
+    }
+    
+    @Override
+    public void start(Stage ikkuna) {
+        NumberAxis xAkseli = new NumberAxis(1965, 2015, 5);
+        NumberAxis yAkseli = new NumberAxis();
+        
+        LineChart<Number, Number> viivakaavio = new LineChart<>(xAkseli, yAkseli);
+        
+        ArrayList<String> data = new ArrayList<>();
+        try (Scanner tiedostonlukija = new Scanner(new File("puoluedata.tsv"))) {
+            while (tiedostonlukija.hasNextLine()) {
+                data.add(tiedostonlukija.nextLine());
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Virhe: " + e.getMessage());
+        }
+        
+        HashMap<String, HashMap<Integer, Double>> puolueet = new HashMap<>();
+        String[] puolueVuodet = data.get(0).split("\t");
+        
+        for (int i = 1; i < data.size(); i++) {
+            String[] riviPaloina = data.get(i).split("\t");
+            HashMap<Integer, Double> puolueenSuosio = new HashMap<>();
+            for (int j = 1; j < puolueVuodet.length; j++) {
+                if (!(riviPaloina[j].equals("-"))) {
+                    puolueenSuosio.put(Integer.valueOf(puolueVuodet[j]), Double.valueOf(riviPaloina[j]));
+                }
+            }
+            puolueet.put(riviPaloina[0], puolueenSuosio);
+        }
+        
+        puolueet.keySet().stream().forEach(puolue -> {
+            XYChart.Series xyData = new XYChart.Series();
+            xyData.setName(puolue);
+            puolueet.get(puolue).entrySet().stream().forEach(pari -> {
+                xyData.getData().add(new XYChart.Data(pari.getKey(), pari.getValue()));
+            });
+            viivakaavio.getData().add(xyData);
+        });
+        
+        Scene nakyma = new Scene(viivakaavio);
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+        
     }
 
 }
