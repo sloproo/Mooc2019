4f343a2b3b552c09b0f4e3703855369b2bbe335c
Pyry <Pyry@mustis>
2019-04-29 18:18:38 +0300

    Jee jee

--- osa13-Osa13_08.Hurraa/src/main/java/hurraa/HurraaSovellus.java
+++ osa13-Osa13_08.Hurraa/src/main/java/hurraa/HurraaSovellus.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 
 public class HurraaSovellus extends Application {
@@ -14,6 +15,12 @@ public class HurraaSovellus extends Application {
 
         Button nappi = new Button("Hurraa!");
         pane.setCenter(nappi);
+        
+        AudioClip hurraa = new AudioClip("file:Applause-Yannick_Lemieux.wav");
+        
+        nappi.setOnAction((event) -> {
+            hurraa.play();
+        });
 
 
         Scene scene = new Scene(pane, 600, 400);
