0a021443145ebeaeacfa14a629edee2d8267da68
Pyry <Pyry@mustis>
2019-05-04 23:08:39 +0300

    Puuh. Nämä testailuhommat ovat tylsiä.

--- osa14-Osa14_06.Testausta/src/main/java/testausta/Ohjelma.java
+++ osa14-Osa14_06.Testausta/src/main/java/testausta/Ohjelma.java
@@ -1,15 +1,45 @@
 package testausta;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Ohjelma {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        suorita(lukija);
+        System.out.println(suorita(lukija));
     }
 
     public static String suorita(Scanner lukija) {
-        return "";
+        String syote = "";
+        int oppilaita = 0;
+        int pulloja = 0;
+        
+        while (!syote.equals("-1")) {
+            System.out.println("Montako pulloa oppilas keräsi? (-1 lopettaa)");
+            syote = lukija.nextLine();
+            if (Integer.valueOf(syote) >= 0) {
+                pulloja += Integer.valueOf(syote);
+                oppilaita++;
+            }
+        }
+        
+        return palautusYhteenveto(pulloja, oppilaita);
+    }
+    
+    public static String palautusYhteenveto(int pulloja, int oppilaita) {
+        String keskiarvoString = "";
+        if (pulloja == 0) {
+            keskiarvoString = "Keskiarvoa ei voida laskea";
+        } else {
+            keskiarvoString = "Keskiarvo: " + pulloja * 1.0 / oppilaita;
+        }
+        
+        String palautettava = "Pulloja: " + pulloja + "\n"
+                + "Oppilaita: " + oppilaita + "\n"
+                + keskiarvoString;
+        
+        return palautettava;
     }
 }
--- osa14-Osa14_06.Testausta/src/test/java/testausta/OhjelmaTest.java
+++ osa14-Osa14_06.Testausta/src/test/java/testausta/OhjelmaTest.java
@@ -1,6 +1,46 @@
 package testausta;
 
+import java.util.Scanner;
+import org.junit.Test;
+import org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class OhjelmaTest {
 
     // toteuta tänne testit luokkaa Ohjelma-varten
+    
+    @Test
+    public void pullojenSummat() {
+        assertTrue("Pullojen summa 2 + 3 ei ollut 5", Ohjelma.suorita(new Scanner("2\n3\n-1\n")).contains("Pulloja: 5"));
+        assertTrue("Pullojen summa 3 + 6 + (-3) ei ollut 9", Ohjelma.suorita(new Scanner("3\n6\n-3\n-1\n")).contains("Pulloja: 9"));
+        assertTrue("Pullojen summa 0 + 0 + (-8) ei ollut 0", Ohjelma.suorita(new Scanner("0\n0\n-8\n-1\n")).contains("Pulloja: 0"));
+    }
+    
+    @Test
+    public void oppilaidenMaara() {
+        assertTrue("Oppilaiden määrä pulloilla 2 + 3 ei ollut 2", 
+                Ohjelma.suorita(new Scanner("2\n3\n-1\n")).contains("Oppilaita: 2"));
+        assertTrue("Oppilaiden määrä pulloilla -2 + -3 ei ollut 0", 
+                Ohjelma.suorita(new Scanner("-15\n-3\n-1\n")).contains("Oppilaita: 0"));
+        assertTrue("Oppilaiden määrä pulloilla 2 + 3 + 5 + 12 ei ollut 4", 
+                Ohjelma.suorita(new Scanner("2\n3\n5\n12\n-1\n")).contains("Oppilaita: 4"));
+        assertTrue("Oppilaiden määrä pulloilla 2 + 3 + 5 + 12 + -55 ei ollut 4", 
+                Ohjelma.suorita(new Scanner("2\n3\n5\n12\n-55\n-1\n")).contains("Oppilaita: 4"));
+        
+    }
+    
+    @Test
+    public void keskiarvoTesti() {
+        assertTrue("Keskiarvo pulloilla 1 + 3 ei ollut 2.0", 
+            Ohjelma.suorita(new Scanner("1\n3\n-1\n")).contains("Keskiarvo: 2.0"));
+        assertTrue("Keskiarvo pulloilla 1 + 0 + 0 + 0 ei ollut 0.25", 
+            Ohjelma.suorita(new Scanner("1\n0\n0\n0\n-1\n")).contains("Keskiarvo: 0.25"));
+        assertTrue("Keskiarvo pulloilla 1 + 3 + -5 ei ollut 2.0", 
+            Ohjelma.suorita(new Scanner("1\n3\n-5\n-1\n")).contains("Keskiarvo: 2.0"));
+        assertTrue("Tilanteessa, jossa palautettuja pulloja ei ollut, palautetta "
+                + "\"Keskiarvoa ei voida laskea\" ei annettu.", 
+            Ohjelma.suorita(new Scanner("0\n0\n-9\n-1\n")).contains("Keskiarvoa ei voida laskea"));
+    }
+    
 }
