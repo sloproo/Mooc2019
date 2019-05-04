2fd10b7a07d3264d7b5c5ae66e5355379683cfa0
Pyry <Pyry@mustis>
2019-05-04 16:45:53 +0300

    Tämä oli nopea.

--- osa14-Osa14_05.Tietokanta/src/main/java/sovellus/Kayttoliittyma.java
+++ osa14-Osa14_05.Tietokanta/src/main/java/sovellus/Kayttoliittyma.java
@@ -30,9 +30,56 @@ public class Kayttoliittyma {
             }
 
             // toteuta toiminnallisuus tänne
+            if (komento.equals("1")) {
+                listaa();
+            }
+            else if (komento.equals("2")) {
+                lisaa();
+            }
+            else if (komento.equals("3")) {
+                asetaTehdyksi();
+            }
+            else if (komento.equals("4")) {
+                poista();
+            }
         }
 
         System.out.println("Kiitos!");
+    }
+
+    private void listaa() throws SQLException {
+        System.out.println("Listataan tietokannan tiedot");
+        for (Todo todo : tietokanta.listaa()) {
+            System.out.println(todo);
+        }
+        System.out.println("");
+    }
+
+    private void lisaa() throws SQLException {
+        System.out.println("Lisätään tehtävää");
+        System.out.println("Syötä nimi");
+        String syotettavanNimi = lukija.nextLine();
+        System.out.println("Syötä kuvaus");
+        String syotettavanKuvaus = lukija.nextLine();
+        Todo lisattava = new Todo(syotettavanNimi, syotettavanKuvaus, false);
+        tietokanta.lisaa(lisattava);
+        System.out.println("");
+    }
+
+    private void asetaTehdyksi() throws SQLException {
+        System.out.println("");
+        System.out.println("Mikä asetetaan tehdyksi (syötä id)?");
+        int tehdyksiAsetettava = Integer.valueOf(lukija.nextLine());
+        tietokanta.asetaTehdyksi(tehdyksiAsetettava);
+        System.out.println("");
+    }
+
+    private void poista() throws SQLException {
+        System.out.println("");
+        System.out.println("Mikä poistetaan (syötä id)?");
+        int poistettavanId = Integer.valueOf(lukija.nextLine());
+        tietokanta.poista(poistettavanId);
+        System.out.println("");
     }
 
 }
